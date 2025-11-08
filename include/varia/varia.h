44ab55b8c4bf9a30e6784ed984ec9df98c610107
potatislc <lucalbin@gmail.com>
2025-11-08 14:29:44 +0100

    Extracted "Construct_"-prefix for varia objects into VARIA_OBJECT macro

--- include/varia/varia.h
+++ include/varia/varia.h
@@ -28,41 +28,43 @@ namespace varia::detail {
     class_name(class_name&&) = default; \
     class_name& operator=(class_name&&) = default;
 
+#define VARIA_OBJECT(class_name) Construct_##class_name
+
 #define varclass(name, body, ...) \
-    class Construct_##name __VA_OPT__( : public Construct_##__VA_ARGS__ )  { \
+    class VARIA_OBJECT(name) __VA_OPT__( : public VARIA_OBJECT(__VA_ARGS__) )  { \
     public: \
-        static_assert(varia::detail::is_single_inheritance_v<__VA_OPT__(Construct_##__VA_ARGS__)>, \
+        static_assert(varia::detail::is_single_inheritance_v<__VA_OPT__(VARIA_OBJECT(__VA_ARGS__))>, \
             "varia: varclass cannot inherit from multiple types: "#__VA_ARGS__); \
         \
-        using name = varia::var<Construct_##name>; \
+        using name = varia::var<VARIA_OBJECT(name)>; \
         \
-        Construct_##name() = default; \
-        virtual ~Construct_##name() = default; \
-        VARIA_DEFAULT_COPY_MOVE_CONSTRUCTORS(Construct_##name) \
+        VARIA_OBJECT(name)() = default; \
+        virtual ~VARIA_OBJECT(name)() = default; \
+        VARIA_DEFAULT_COPY_MOVE_CONSTRUCTORS(VARIA_OBJECT(name)) \
         \
         body \
     }; \
-    using name = varia::var<Construct_##name, SharedStorage>;
+    using name = varia::var<VARIA_OBJECT(name), SharedStorage>;
 
 #define varstruct(name, body, ...) \
-    struct Construct_##name __VA_OPT__( : public Construct_##__VA_ARGS__ ) { \
-        static_assert(varia::detail::is_single_inheritance_v<__VA_OPT__(Construct_##__VA_ARGS__)>, \
+    struct VARIA_OBJECT(name) __VA_OPT__( : public VARIA_OBJECT(__VA_ARGS__) ) { \
+        static_assert(varia::detail::is_single_inheritance_v<__VA_OPT__(VARIA_OBJECT(__VA_ARGS__))>, \
             "varia: varstruct cannot inherit from multiple types: "#__VA_ARGS__); \
         \
         template<typename> \
         static constexpr auto check_polymorphic_bases = []<typename... Args> { \
             static_assert((!std::is_polymorphic_v<Args> && ...), \
                 "varia: varstruct cannot inherit from a polymorphic type (such as a varclass): "#__VA_ARGS__); \
-        }.template operator()<__VA_OPT__(Construct_##__VA_ARGS__)>(); \
+        }.template operator()<__VA_OPT__(VARIA_OBJECT(__VA_ARGS__))>(); \
         \
-        using name = varia::var<Construct_##name>; \
+        using name = varia::var<VARIA_OBJECT(name)>; \
         \
-        Construct_##name() = default; \
-        ~Construct_##name() = default; \
-        VARIA_DEFAULT_COPY_MOVE_CONSTRUCTORS(Construct_##name) \
+        VARIA_OBJECT(name)() = default; \
+        ~VARIA_OBJECT(name)() = default; \
+        VARIA_DEFAULT_COPY_MOVE_CONSTRUCTORS(VARIA_OBJECT(name)) \
         \
         body \
     }; \
-    static_assert(!std::is_polymorphic_v<Construct_##name>); \
+    static_assert(!std::is_polymorphic_v<VARIA_OBJECT(name)>); \
     \
-    using name = varia::var<Construct_##name, CopiedStorage>;
+    using name = varia::var<VARIA_OBJECT(name), CopiedStorage>;
